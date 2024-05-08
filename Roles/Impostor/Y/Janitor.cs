ad07d5e98e71060da6c1a3cdd763aeaf5fab9a92
Kihi1120 <hope.light1120@gmail.com>
2024-05-09 00:06:58 +0900

    <作業途中.wip> 　相方のインポスターがキルを行う時に、 　近くにJanitorがいる場合キルを防ぐ処理を追加。
    　キルを防いだ後にJanitorがその対象者に
    　キルボタンを押すと、対象者が透明化する処理を追加。
    　
    　5秒後に元の姿へ戻る処理を追加。
    　※完全に戻る訳では無く、幽霊から元に戻る感じ。
    　→周りからは見えないままです。
    
    　BackBodyのメソッドで全員に見えるような処理を追加。
    
    　＿＿＿＿＿＿＿＿＿＿＿＿＿
    　続きの作業memo
    　　○透明化したプレイヤーのタスクを無くす。

--- Roles/Impostor/Y/Janitor.cs
+++ Roles/Impostor/Y/Janitor.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using AmongUs.GameOptions;
 
 using TownOfHostY.Roles.Core;
@@ -26,6 +27,10 @@ public sealed class Janitor : RoleBase, IImpostor
     {
         CleanCooldown = OptionCleanCooldown.GetFloat();
         LookJanitor = OptionLookJanitor.GetFloat();
+        JanitorTarget = byte.MaxValue;
+        JanitorChance = false;
+        ReturnBody = false;
+        CleanPlayer.Clear();
     }
     private static OptionItem OptionCleanCooldown;
     private static OptionItem OptionLookJanitor;
@@ -36,17 +41,44 @@ public sealed class Janitor : RoleBase, IImpostor
     }
     private static float CleanCooldown;
     private static float LookJanitor;
+    public static byte JanitorTarget;
+    private static bool JanitorChance;
+    private static bool ReturnBody;
+    Dictionary<byte, object> CleanPlayer = new(14);
     private static void SetUpOptionItem()
     {
         OptionCleanCooldown = FloatOptionItem.Create(RoleInfo, 10, OptionName.JanitorCleanCooldown, new(5.0f, 900f, 2.5f), 10f, false)
-            .SetValueFormat(OptionFormat.Seconds);
+            .SetValueFormat(OptionFormat.Seconds);//掃除のクールダウン
         OptionLookJanitor = FloatOptionItem.Create(RoleInfo, 11, OptionName.LookJanitor, new(1.0f, 5f, 0.5f), 2f, false)
-        .SetValueFormat(OptionFormat.Multiplier);
+        .SetValueFormat(OptionFormat.Multiplier);//Janitorの距離
     }
     public override void ApplyGameOptions(IGameOptions opt) => AURoleOptions.ShapeshifterCooldown = CleanCooldown;
     public void OnCheckMurderAsKiller(MurderInfo info)
     {
+        (var killer, var target) = info.AttemptTuple;
         info.DoKill = false;
+        if (JanitorChance)
+        {
+            if (JanitorTarget == target.PlayerId)
+            {
+                target.RpcSetPet("");// 変身先のプレイヤーのペットを非表示にする
+                target.Data.IsDead = true;
+                AntiBlackout.SendGameData();
+                Utils.NotifyRoles(ForceLoop: true);
+                if (!CleanPlayer.ContainsKey(target.PlayerId))
+                {
+                    CleanPlayer.Add(target.PlayerId, null);
+                }
+
+                _ = new LateTask(() =>
+                {
+                    target.Data.IsDead = false;
+                    AntiBlackout.SendGameData();
+                    target.SetKillCooldown(2.5f);
+                    target.RpcResetAbilityCooldown();
+                }, 5, "ReturnBody");
+            }
+        }
     }
     public static bool GuardPlayerCheckMurder(MurderInfo info)
     {
@@ -64,10 +96,55 @@ public sealed class Janitor : RoleBase, IImpostor
 
                     killer.RpcProtectedMurderPlayer(target); //killer側のみ。斬られた側は見れない。
                     info.CanKill = false;
+                    JanitorTarget = target.PlayerId;
+                    JanitorChance = true;
                     break; // Janitorが見つかったらループを終了
                 }
             }
         }
         return true;
     }
+    public override void AfterMeetingTasks()
+    {
+        JanitorChance = false;
+    }
+    public override void OnReportDeadBody(PlayerControl _, GameData.PlayerInfo __)
+    {
+        ReturnBody = true;
+        foreach (var targetId in CleanPlayer.Keys)
+        {
+            var target = Utils.GetPlayerById(targetId);
+            if (ReturnBody)//元の姿に戻す処理。
+            {
+                Utils.NotifyRoles(ForceLoop: true);
+                target.SetKillCooldown(Options.DefaultKillCooldown);
+                target.RpcResetAbilityCooldown();
+            }
+            KillClean(target, true);
+            BackBody();
+            JanitorChance = false;
+        }
+        CleanPlayer.Clear();
+    }
+    private void KillClean(PlayerControl target, bool isButton = false)
+    {
+        var Janitor = Player;
+        target.SetRealKiller(Janitor);
+        CustomRoleManager.OnCheckMurder(
+            Janitor, target,
+            target, target
+        );
+        Logger.Info($"Janitorに消されている{target.name}をキルしました。", "Janitor.KillClean");
+        if (!isButton && Janitor.IsAlive())
+        {
+            RPC.PlaySoundRPC(Janitor.PlayerId, Sounds.KillSound);
+        }
+    }
+    public void BackBody()
+    {
+        Utils.NotifyRoles(ForceLoop: true);
+        Player.SetKillCooldown(Options.DefaultKillCooldown);
+        Player.RpcResetAbilityCooldown();
+    }
+
 }
