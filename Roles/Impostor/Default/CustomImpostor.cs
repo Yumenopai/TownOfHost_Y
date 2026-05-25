11c2d47b6d3bbbdaa091191a2918da4be421a9ac
Tabasco1410 <traine233.yk@gmail.com>
2026-05-25 11:36:54 +0900

    正しく変更ができていなかった問題の修正

--- Roles/Impostor/Default/CustomImpostor.cs
+++ Roles/Impostor/Default/CustomImpostor.cs
@@ -40,7 +40,7 @@ public sealed class CustomImpostor : RoleBase, IImpostor
     {
         OptionKillCooldown = FloatOptionItem.Create(RoleInfo, 10, GeneralOption.KillCooldown, new(0f, 180f, 2.5f), 30f, false)
             .SetValueFormat(OptionFormat.Seconds);
-        OptionCanVent = StringOptionItem.Create(RoleInfo, 11, GeneralOption.CanVent, new[] { "ColoredOff", "ColoredON" }, 1, false);
+        OptionCanVent = StringOptionItem.Create(RoleInfo, 11, GeneralOption.CanVent, new[] { "ColoredOff", "ColoredOn" }, 1, false);
         OptionCanUseSabotage = BooleanOptionItem.Create(RoleInfo, 12, GeneralOption.CanUseSabotage, true, false);
         OptionHasImpostorVision = BooleanOptionItem.Create(RoleInfo, 13, GeneralOption.ImpostorVision, true, false);
         Options.SetUpAddOnOptions(RoleInfo.ConfigId + 20, RoleInfo.RoleName, RoleInfo.Tab);
